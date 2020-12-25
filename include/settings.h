b9ab69f2285542746dc0dbd2a9ad14de0dba29bc
thibclz <thibaut.caillierez@gmail.com>
2020-12-25 01:12:21 +0100

    credit added

--- include/settings.h
+++ include/settings.h
@@ -1,3 +1,12 @@
-extern float output_frequency = 800;
+/*
+This file contains every global variables and settingyou might want
+to change
 
-extern float tick_time = 0.1;
+*/
+
+
+//OUTPUT
+
+extern float output_frequency = 800; //frequency of the morse wav, must stay under 4000Hz in order to respect Shannon's criteria
+
+extern float tick_time = 0.1; //time of a beep
